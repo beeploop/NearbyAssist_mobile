fd932366b93af67e54cffe318f04bed1b379ae15
John Loyd Mulit <jlmulit68@gmail.com>
2024-10-22 12:45:40 +0800

    chore: remove unused code

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,6 +1,5 @@
 import 'package:latlong2/latlong.dart';
 import 'package:nearby_assist/model/request/facebook_login_response.dart';
-import 'package:nearby_assist/model/service_model.dart';
 import 'package:nearby_assist/model/tag_model.dart';
 
 const defaultLocation = LatLng(7.422365, 125.825984);
@@ -12,33 +11,6 @@ final fakeUser = FacebookLoginResponse(
   email: "ajuntilla@gmail.com",
   image: "https://graph.facebook.com/10219118101307344/picture",
 );
-
-final mockLocations = [
-  Service(
-    id: "1",
-    score: 0.0,
-    rank: 200,
-    vendor: 'Hugo',
-    latitude: 7.424373,
-    longitude: 125.829315,
-  ),
-  Service(
-    id: "2",
-    score: 0.0,
-    rank: 100,
-    vendor: '11:11',
-    latitude: 7.422427,
-    longitude: 125.824767,
-  ),
-  Service(
-    id: "3",
-    score: 0.0,
-    rank: 300,
-    vendor: 'Cyan',
-    latitude: 7.419195,
-    longitude: 125.824142,
-  ),
-];
 
 final validId = [
   "Driver's License",
