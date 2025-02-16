c54c91bcb4b00babfbe5ab40b7ae92f8a8c77d57
John Loyd Mulit <jlmulit68@gmail.com>
2025-02-17 07:02:06 +0800

    chore: bump patch version

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -2,7 +2,7 @@ import 'package:latlong2/latlong.dart';
 import 'package:nearby_assist/models/login_payload_model.dart';
 import 'package:nearby_assist/models/user_model.dart';
 
-const appVersion = "v0.10.0-alpha";
+const appVersion = "v0.10.1-alpha";
 const appName = "NearbyAssist";
 const appLegalese = "© 2024 NearbyAssist";
 
