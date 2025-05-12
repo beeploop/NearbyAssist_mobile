f97ca120376e003a05ab7515aeb0c51cf88e900d
John Loyd Mulit <jlmulit68@gmail.com>
2025-05-12 10:23:26 +0800

    bump version

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,7 +1,7 @@
 import 'package:latlong2/latlong.dart';
 import 'package:nearby_assist/models/user_model.dart';
 
-const appVersion = "v0.15.1-alpha";
+const appVersion = "v0.16.0-alpha";
 const appName = "NearbyAssist";
 const appLegalese = "© 2024 NearbyAssist";
 
