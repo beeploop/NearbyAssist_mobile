6437e4e36055d3f15baf9456aed4f19c44926889
John Loyd Mulit <jlmulit68@gmail.com>
2025-05-13 10:53:46 +0800

    prepare release candidate

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,7 +1,7 @@
 import 'package:latlong2/latlong.dart';
 import 'package:nearby_assist/models/user_model.dart';
 
-const appVersion = "v0.17.0-alpha";
+const appVersion = "v0.17.0-rc1";
 const appName = "NearbyAssist";
 const appLegalese = "© 2024 NearbyAssist";
 
